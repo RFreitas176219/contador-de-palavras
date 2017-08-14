7a1e9efc2c4d8b57f878afd5ff027c3ade712d80
Rafael <rafaelpfreitas@hotmail.com>
2017-08-14 10:17:48 -0300

    final

--- src/main.c
+++ src/main.c
@@ -6,17 +6,86 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(){
+	int i,aux,fim,inicio=0,total=1;
+	char frase[1000];
+	
 
-  char c;
+	scanf("%[^\n]", frase);
+	fim = strlen(frase);
+	
+	while(frase[inicio] != '\0'){
+		if(frase[inicio] == '0' | frase[inicio] == '1' | frase[inicio] == '2' |
+                   frase[inicio] == '3' | frase[inicio] == '4' | frase[inicio] == '5' |
+                   frase[inicio] == '6' | frase[inicio] == '7' | frase[inicio] == '8' |
+                   frase[inicio] == '9' )
+			if(frase[inicio+1] == '.' | frase[inicio+1] == ',' )
+				if(frase[inicio+2] == '0' | frase[inicio+2] == '1' |
+		                   frase[inicio+2] == '2' | frase[inicio+2] == '3' |	
+                                   frase[inicio+2] == '4' | frase[inicio+2] == '5' |
+                                   frase[inicio+2] == '6' | frase[inicio+2] == '7' |
+                                   frase[inicio+2] == '8' | frase[inicio+2] == '9' ){
+					frase[inicio+1] = ' ';
+					i = inicio+1;
+					while(frase[i] != '\0'){
+					frase[i] = frase[i+1];
+					i++;
+					}
+				}
+	inicio++;
+	}
+	
+	inicio=0;
+	while(frase[inicio] != '\0'){
+		switch(frase[inicio]){
+			case '.':
+				frase[inicio] = ' ';
+				break;
+			case ',':
+				frase[inicio] = ' ';
+				break;	
+			case ':':
+				frase[inicio] = ' ';
+				break;	
+			case ';':
+				frase[inicio] = ' ';
+				break;	
+			case '"':
+				frase[inicio] = ' ';
+				break;	
+			case '!':
+				frase[inicio] = ' ';
+				break;	
+			case '?':
+				frase[inicio] = ' ';
+				break;	
+			case '(':
+				frase[inicio] = ' ';
+				break;	
+			case ')':
+				frase[inicio] = ' ';
+				break;	
+			case '\'':
+				frase[inicio] = ' ';
+				break;
+			case '-':
+				frase[inicio] = ' ';
+				break;
+		}
+		inicio++;
+	}
 
-  c = 1;
+	fim = strlen(frase);
+	for(inicio=0; inicio<=fim; inicio++){
+		if((frase[inicio] == ' ') && (frase[inicio+1] != '\0') && (frase[inicio+1] != ' '))
+			total++;
+	}
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+	printf("%d\n", total);
 
-  printf("1\n");
-  return 0;
-}
+	return 0;
+}		
+
